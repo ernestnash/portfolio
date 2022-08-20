93dc0f6ed012cfab9ea702ed5bda69fbbe5e33f5
ernestnash <eernestnnashon@gmail.com>
2022-08-20 23:00:25 +0300

    My personal Portfolio

---
+++ assets/javascript/index.js
@@ -0,0 +1,16 @@
+// nashon ernest
+// nashon ernest
+// nashon ernest
+// nashon ernest
+
+
+// Page animation
+AOS.init({
+    duration: 1000,
+}); 
+
+
+// nashon ernest
+// nashon ernest
+// nashon ernest
+// nashon ernest
